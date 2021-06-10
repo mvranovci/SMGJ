80a7e9d5348f9c4fa774806847e27087e2ac2773
Miranda Vranovci <miranda.vranovci1@gmail.com>
2021-06-10 15:40:08 +0200

    Perfundimi i CRUD operacionet

--- SMGJ/Controllers/MENUController.cs
+++ SMGJ/Controllers/MENUController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace SMGJ.Controllers
 {
@@ -24,14 +25,39 @@ namespace SMGJ.Controllers
             MENU model = new MENU();
             return View(model);
         }
+        // DeleteMenu
+        [HttpPost]
+        public async Task<ActionResult> DeleteMenu(MENU model)
+        {
+            var user = await GetUser();
+            MessageJs returnmodel = new MessageJs();
+            try
+            {
+                MENU menu =   db.MENUs.Find(model.ID);
+                // detyre:    Nese  menu-ja e gjetur egziston tek tabela e MENU_ROLI
+                //le t del mesazh qe nuk muni me fshi kete tabel
+
+                //Detyra 2: Me dal me konfirmu para me se mujt me fshi nje menu
+                db.MENUs.Remove(menu);
+                await db.SaveChangesAsync();
+                returnmodel.status = true; 
+                return Json(returnmodel, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                returnmodel.status = false; 
+                returnmodel.Mesazhi = "Ka ndodhur nje gabim";
+                return Json(returnmodel, JsonRequestBehavior.DenyGet);
+            }  
+        }
         public async Task<ActionResult> Edit(int? id)
         {
             var user = await GetUser();
             MENU model = new MENU();
-            if(id != null)
+            if (id != null)
             {
                 model = db.MENUs.Find(id.Value);
-            } 
+            }
             return View(model);
         }
         [HttpPost]
@@ -40,38 +66,76 @@ namespace SMGJ.Controllers
             var user = await GetUser();
             MessageJs returnmodel = new MessageJs();
             if (ModelState.IsValid)
-            { 
+            {
                 try
                 {
-                    MENU new_model = new MENU();
+                    MENU new_model = new MENU();  
 
                     new_model.Controller = model.Controller;
                     new_model.Action_Metoda = model.Action_Metoda;
                     new_model.Aktiv = model.Aktiv;
                     new_model.Emertimi = model.Emertimi;
                     new_model.Renditja = model.Renditja;
-                    db.MENUs.Add(new_model); 
-                    await db.SaveChangesAsync(); 
+                    db.MENUs.Add(new_model);
+                    await db.SaveChangesAsync();
                     returnmodel.status = true;
                     returnmodel.Mesazhi = "Menu-ja u regjistrua me sukses";
                     return Json(returnmodel, JsonRequestBehavior.AllowGet);
                 }
                 catch
-                {    
+                {
                     returnmodel.status = false;
                     returnmodel.Mesazhi = "Ka ndodhur nje gabim";
                     return Json(returnmodel, JsonRequestBehavior.AllowGet);
-                } 
+                }
             }
             else
-            { 
+            {
                 returnmodel.status = false;
                 returnmodel.Mesazhi = "Modeli nuk eshte valid";
                 return Json(returnmodel, JsonRequestBehavior.DenyGet);
             }
         }
 
-            
+        [HttpPost]
+        public async Task<ActionResult> Edit(MENU model)
+        {
+            var user = await GetUser();
+            MessageJs returnmodel = new MessageJs();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    MENU new_model = db.MENUs.Find(model.ID);
+
+                    new_model.Controller = model.Controller;
+                    new_model.Action_Metoda = model.Action_Metoda;
+                    new_model.Aktiv = model.Aktiv;
+                    new_model.Emertimi = model.Emertimi;
+                    new_model.Renditja = model.Renditja;
+                    //bone update
+                    db.Entry(new_model).State = EntityState.Modified;
+                    //ruaj te dhenat
+                    await db.SaveChangesAsync();
+                    returnmodel.status = true;
+                    returnmodel.Mesazhi = "Menu-ja u editua me sukses";
+                    return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                    return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                }
+                catch
+                {
+                    returnmodel.status = false;
+                    returnmodel.Mesazhi = "Ka ndodhur nje gabim";
+                    return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                returnmodel.status = false;
+                returnmodel.Mesazhi = "Modeli nuk eshte valid";
+                return Json(returnmodel, JsonRequestBehavior.DenyGet);
+            }
+        }
 
     }
 }
