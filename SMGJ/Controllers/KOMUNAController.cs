ac7f1300872ec5f07b2068366a8e02a22bc6b687
LirimM <34185066+LirimM@users.noreply.github.com>
2021-06-15 11:26:46 +0200

    Add files via upload

---
+++ SMGJ/Controllers/KOMUNAController.cs
@@ -0,0 +1,163 @@
+﻿using SMGJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace SMGJ.Controllers
+{
+    public class KOMUNAController : BaseController
+    {
+        SMGJDB db = new SMGJDB();
+        // GET: KOMUNA
+        public async Task<ActionResult> Index()
+        {
+            var user = await GetUser();
+            List<KOMUNA> model = db.KOMUNAs.ToList();
+            return View(model);
+        }
+        public async Task<ActionResult> Create()
+        {
+            var user = await GetUser();
+            KOMUNA model = new KOMUNA();
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(KOMUNA model)
+        {
+            var user = await GetUser();
+            MessageJs returnmodel = new MessageJs();
+
+            var exists = db.KOMUNAs.Any(x => x.Emri == model.Emri);
+            if (exists)
+            {
+                returnmodel.status = false;
+                returnmodel.Mesazhi = "Nuk mund ta regjistroni kete komune sepse ekziston!";
+                return Json(returnmodel, JsonRequestBehavior.AllowGet);
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    KOMUNA new_model = new KOMUNA();
+
+                    
+                    new_model.Emri = model.Emri;
+                    new_model.Kodi = model.Kodi;
+                    new_model.Krijuar = DateTime.Now;
+                    new_model.KrijuarNga = user.ID;
+
+                    db.KOMUNAs.Add(new_model);
+                    await db.SaveChangesAsync();
+                    returnmodel.status = true;
+                    returnmodel.Mesazhi = "Komuna u regjistrua me sukses";
+                    return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                }
+                catch
+                {
+                    returnmodel.status = false;
+                    returnmodel.Mesazhi = "Ka ndodhur nje gabim";
+                    return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                returnmodel.status = false;
+                returnmodel.Mesazhi = "Modeli nuk eshte valid";
+                return Json(returnmodel, JsonRequestBehavior.DenyGet);
+            }
+        }
+
+        //DeleteMenu
+        [HttpPost]
+        public async Task<ActionResult> DeleteKomuna(KOMUNA model)
+        {
+            var user = await GetUser();
+            MessageJs returnmodel = new MessageJs();
+            try
+            {
+                KOMUNA komuna = db.KOMUNAs.Find(model.ID);
+                var existsFerma = await db.FERMAs.AnyAsync(x => x.KomunaID == model.ID);
+                var existsUsers = await db.USERs.AnyAsync(x => x.KomunaID == model.ID);
+                if (existsFerma || existsUsers)
+                {
+                    returnmodel.status = false;
+                    returnmodel.Mesazhi = "Kete komune nuk mund ta fshini sepse gjendet ne tabelen FERMA ose USER!";
+                    return Json(returnmodel, JsonRequestBehavior.DenyGet);
+                }
+
+                db.KOMUNAs.Remove(komuna);
+                await db.SaveChangesAsync();
+                returnmodel.status = true;
+                returnmodel.Mesazhi = "Komuna eshte fshire!";
+                return Json(returnmodel, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                returnmodel.status = false;
+                returnmodel.Mesazhi = "Ka ndodhur nje gabim";
+                return Json(returnmodel, JsonRequestBehavior.DenyGet);
+            }
+        }
+        public async Task<ActionResult> Edit(int? id)
+        {
+            var user = await GetUser();
+            KOMUNA model = new KOMUNA();
+            if (id != null)
+            {
+                model = db.KOMUNAs.Find(id.Value);
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(KOMUNA model)
+        {
+            var user = await GetUser();
+            MessageJs returnmodel = new MessageJs();
+            var exists = db.KOMUNAs.Any(x => x.Emri == model.Emri);
+            if (exists)
+            {
+                returnmodel.status = false;
+                returnmodel.Mesazhi = "Nuk mund ta ndryshoni kete komune sepse ekziston!";
+                return Json(returnmodel, JsonRequestBehavior.DenyGet);
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    KOMUNA new_model = db.KOMUNAs.Find(model.ID);
+
+                    new_model.Emri = model.Emri;
+                    new_model.Kodi = model.Kodi;
+                    new_model.Krijuar = DateTime.Now;
+                    new_model.KrijuarNga = user.ID;
+                    //bone update
+                    db.Entry(new_model).State = EntityState.Modified;
+                    //ruaj te dhenat
+                    await db.SaveChangesAsync();
+                    returnmodel.status = true;
+                    returnmodel.Mesazhi = "Komuna u editua me sukses";
+                    return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                    //return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                }
+                catch
+                {
+                    returnmodel.status = false;
+                    returnmodel.Mesazhi = "Ka ndodhur nje gabim";
+                    return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                returnmodel.status = false;
+                returnmodel.Mesazhi = "Modeli nuk eshte valid";
+                return Json(returnmodel, JsonRequestBehavior.DenyGet);
+            }
+        }
+
+    }
+}
---
+++ SMGJ/Controllers/RRAHJET_ZEMRESController.cs
@@ -0,0 +1,161 @@
+﻿using SMGJ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace SMGJ.Controllers
+{
+    public class RRAHJET_ZEMRESController : BaseController
+    {
+        SMGJDB db = new SMGJDB();
+        // GET: RRAHJET_ZEMRES
+        public async Task<ActionResult> Index()
+        {
+            var user = await GetUser();
+            List<RRAHJET_ZEMRES> model = db.RRAHJET_ZEMRES.ToList();
+            return View(model);
+        }
+        public async Task<ActionResult> Create()
+        {
+            var user = await GetUser();
+            RRAHJET_ZEMRES model = new RRAHJET_ZEMRES();
+            return View(model);
+        }
+        //DeleteMenu
+        [HttpPost]
+        public async Task<ActionResult> DeleteRrahjetZemres(RRAHJET_ZEMRES model)
+        {
+            var user = await GetUser();
+            MessageJs returnmodel = new MessageJs();
+            try
+            {
+                RRAHJET_ZEMRES rrahjet_zemres = db.RRAHJET_ZEMRES.Find(model.ID);
+
+                if (await db.GJEDHAT_PARAMETRAT.AnyAsync(x => x.RrahjeteZemresID == model.ID))
+                {
+                    returnmodel.status = false;
+                    returnmodel.Mesazhi = "Kete vlere nuk mund ta fshini sepse gjendet ne tabelen GJEDHAT PARAMETRAT!";
+
+                    return Json(returnmodel, JsonRequestBehavior.DenyGet);
+                }
+                
+                db.RRAHJET_ZEMRES.Remove(rrahjet_zemres);
+                await db.SaveChangesAsync();
+                returnmodel.status = true;
+                returnmodel.Mesazhi = "Vlera eshte fshire!";
+                return Json(returnmodel, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+                returnmodel.status = false;
+                returnmodel.Mesazhi = "Ka ndodhur nje gabim";
+                return Json(returnmodel, JsonRequestBehavior.DenyGet);
+            }
+        }
+        public async Task<ActionResult> Edit(int? id)
+        {
+            var user = await GetUser();
+            RRAHJET_ZEMRES model = new RRAHJET_ZEMRES();
+            if (id != null)
+            {
+                model = db.RRAHJET_ZEMRES.Find(id.Value);
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(RRAHJET_ZEMRES model)
+        {
+            var user = await GetUser();
+            MessageJs returnmodel = new MessageJs();
+            var exists = db.GJEDHAT_PARAMETRAT.Any(gj => gj.RrahjeteZemresID == model.ID);
+            var existsEmertimi = db.RRAHJET_ZEMRES.Any(x => x.Vlera == model.Vlera);
+            if (exists || existsEmertimi)
+            {
+                returnmodel.status = false;
+                returnmodel.Mesazhi = "Vlera ekziston te gjedhi parametrat ose te rrahjet e zemres!";
+                return Json(returnmodel, JsonRequestBehavior.DenyGet);
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    RRAHJET_ZEMRES new_model = new RRAHJET_ZEMRES();
+
+                    new_model.Vlera = model.Vlera;
+                    new_model.Krijuar = DateTime.Now;
+                    new_model.KrijuarNga = user.ID;
+
+                    db.RRAHJET_ZEMRES.Add(new_model);
+                    await db.SaveChangesAsync();
+                    returnmodel.status = true;
+                    returnmodel.Mesazhi = "Rrahjet e zemres u regjistruan me sukses";
+                    return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                }
+                catch
+                {
+                    returnmodel.status = false;
+                    returnmodel.Mesazhi = "Ka ndodhur nje gabim";
+                    return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                returnmodel.status = false;
+                returnmodel.Mesazhi = "Modeli nuk eshte valid";
+                return Json(returnmodel, JsonRequestBehavior.DenyGet);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(RRAHJET_ZEMRES model)
+        {
+            var user = await GetUser();
+            MessageJs returnmodel = new MessageJs();
+
+            var exists = db.GJEDHAT_PARAMETRAT.Any(gj => gj.RrahjeteZemresID == model.ID);
+            var existsEmertimi = db.RRAHJET_ZEMRES.Any(x => x.Vlera == model.Vlera);
+            if (exists || existsEmertimi)
+            {
+                returnmodel.status = false;
+                returnmodel.Mesazhi = "Vlera ekziston te gjedhi parametrat ose te rrahjet e zemres!";
+                return Json(returnmodel, JsonRequestBehavior.DenyGet);
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    RRAHJET_ZEMRES new_model = db.RRAHJET_ZEMRES.Find(model.ID);
+
+                    new_model.Vlera = model.Vlera;
+                    new_model.Krijuar = DateTime.Now;
+                    new_model.KrijuarNga = user.ID;
+                    //bone update
+                    db.Entry(new_model).State = EntityState.Modified;
+                    //ruaj te dhenat
+                    await db.SaveChangesAsync();
+                    returnmodel.status = true;
+                    returnmodel.Mesazhi = "Rrahjet e zemres u edituan me sukses";
+                    return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                    //return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                }
+                catch
+                {
+                    returnmodel.status = false;
+                    returnmodel.Mesazhi = "Ka ndodhur nje gabim";
+                    return Json(returnmodel, JsonRequestBehavior.AllowGet);
+                }
+            }
+            else
+            {
+                returnmodel.status = false;
+                returnmodel.Mesazhi = "Modeli nuk eshte valid";
+                return Json(returnmodel, JsonRequestBehavior.DenyGet);
+            }
+        }
+
+    }
+}
