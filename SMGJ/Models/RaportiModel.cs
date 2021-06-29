fb81ede723481907aa8709354240ba056b5d1a2e
LirimM <34185066+LirimM@users.noreply.github.com>
2021-06-29 14:56:58 +0200

    Add files via upload

---
+++ SMGJ/Models/RaportiModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMGJ.Models
+{
+    public class RaportiModel
+    {
+        public int Raportet { get; set; }
+        public int FermaID { get; set; }
+        public DateTime Prej { get; set; }
+        public DateTime Deri { get; set; }
+    }
+}
